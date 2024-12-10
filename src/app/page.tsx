84aa8eb24e7b243035c0ba90d9dce6c3f9101422
yashpratap <pratapyashkulshrestha@gmail.com>
2024-12-10 21:56:59 +0530

    feat: added shadcn ui library and added some components

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+const Home = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <p>Hello World</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default Home;
