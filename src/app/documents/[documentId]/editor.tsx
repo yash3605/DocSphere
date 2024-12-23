09fa1ec505c4e35e3fc99513a5b5b023f512724e
yashpratap <pratapyashkulshrestha@gmail.com>
2024-12-23 23:07:13 +0530

    feat: added tiptap extensions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,14 @@
 "use client";
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
 
 export const Editor = () => {
   const editor = useEditor({
@@ -11,7 +18,19 @@ export const Editor = () => {
         class: "focus:outline-none border print:border-0 bg-white border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TaskList,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      Image,
+      ImageResize
+    ],
     content: '<p>Hello World! 🌎️</p>',
   })
   return (
